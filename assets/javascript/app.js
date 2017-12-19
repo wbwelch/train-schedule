0bdec8e0831db62233a95589f8373c4b29f6e79c
WB Welch <wbwelch@wbs-macbook-pro.attlocal.net>
2017-12-18 22:23:23 -0600

    File structure created, HTML, CSS started, onclick started

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+$("#sumbit").on("click", function() {
+	event.preventDefault();
+	
+	name = $("#name").val().trim();
+	destination = $("#destination").val().trim();
+	firstTrain = $("#first-time").val().trim();
+	frequency = $("#frequency").val().trim();
+	//21st day index.html for example code
+});
